0c31d08ed4bdd34eeb88f5e5925738c9b3f07a41
liquanhui-99 <55009116+liquanhui-99@users.noreply.github.com>
2023-09-05 13:49:26 +0800

    feat：修改备注和README.md文件

--- type.go
+++ type.go
@@ -12,5 +12,8 @@ type Limiter interface {
 
 // DistributedLimiter 分布式场景下使用的限流器接口
 type DistributedLimiter interface {
+	// Allow 是否允许通过限流器继续请求
+	// key 是存储在Redis中的键，可以是单个接口，也可以是整个服务
+	// 返回true则通过，返回false和error为不通过
 	Allow(ctx context.Context, key string) (bool, error)
 }
